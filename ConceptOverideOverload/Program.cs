5f2ad113cb01e48f3e3bd8b73594b297a3955649
Manali Renavikar <manalirenavikar@gmail.com>
2023-09-29 22:09:46 +0100

    commit all projects

---
+++ ConceptOverideOverload/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptOverideOverload
+{
+
+    public class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string checkName(string ckname)
+        {
+             if(name=="Manali")
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            return name;
+        }
+    }
+
+    class Program
+    {
+        
+    static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            myObj.Name = name;
+        //    Console.WriteLine(myObj.Name);
+           // Console.ReadLine();
+            myObj.checkName(name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodOverload/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        void display(int a)
+        {
+            Console.WriteLine("Arguments: " + a);
+        }
+        void display(int a, int b)
+        {
+            Console.WriteLine("Arguments: " + a + " and " + b);
+        }
+        static void Main(string[] args)
+        {
+           
+            Program p1 = new Program();
+            p1.display(100);
+            p1.display(100, 200);
+            Console.ReadLine();
+        }
+    }
+}
